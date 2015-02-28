c60bc1a254d905680cb82d3a26d889c72eadbd75
nowak <nowak@ashpool.local>
2015-02-28 11:41:50 -0500

    + adding challeng 22

--- set3/main.go
+++ set3/main.go
@@ -34,7 +34,8 @@ func main() {
 	// challenge18()
 	// challenge19()
 	// challenge20()
-	challenge21()
+	// challenge21()
+	challenge22()
 }
 
 func challenge17() {
@@ -464,6 +465,35 @@ func challenge21() {
 	initializeMTGenerator(0)
 	y = extractMTNumber()
 	fmt.Println("MT number (seed=0): ", y)
+}
+
+func challenge22() {
+	// ------------------------------------------------------------
+
+	// 22. "Crack" An MT19937 Seed
+
+	// Make sure your MT19937 accepts an integer seed value. Test it (verify
+	// that you're getting the same sequence of outputs given a seed).
+
+	// Write a routine that performs the following operation:
+
+	// * Wait a random number of seconds between, I don't know, 40 and 1000.
+
+	// * Seeds the RNG with the current Unix timestamp
+
+	// * Waits a random number of seconds again.
+
+	// * Returns the first 32 bit output of the RNG.
+
+	// You get the idea. Go get coffee while it runs. Or just simulate the
+	// passage of time, although you're missing some of the fun of this
+	// exercise if you do that.
+
+	// From the 32 bit RNG output, discover the seed.
+
+	// ------------------------------------------------------------
+	fmt.Println("Challenge 22")
+
 }
 
 ////////////// -----------------------------------------------------
